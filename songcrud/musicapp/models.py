08f742d1121cac380e356e15e14c7aac55b387f6
Dev-wonderful <adeonederful20@gmail.com>
2022-10-29 07:48:12 +0100

    created models for musicapp

---
+++ songcrud/musicapp/models.py
@@ -0,0 +1,18 @@
+from turtle import title
+from django.db import models
+
+# Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    date_released = models.CharField(max_length=50)
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey('Artiste', on_delete=models.CASCADE)
+
+class Lyric(models.Model):
+    content = models.TextField()
+    song_id = models.ForeignKey('Song', on_delete=models.CASCADE)
